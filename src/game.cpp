53577b2c8155c87987fb3e5f1f3005aebf57904d
Maximilian Speicher <maximilian.speicher@tum.de>
2020-04-02 23:09:43 +0200

    Ensuring food is placed on grid
    The random generator for x and y could generate food places which are outside of the grid because it generates from 0 to 32 inclusive. However 32 should not be included.

--- src/game.cpp
+++ src/game.cpp
@@ -5,8 +5,8 @@
 Game::Game(std::size_t grid_width, std::size_t grid_height)
     : snake(grid_width, grid_height),
       engine(dev()),
-      random_w(0, static_cast<int>(grid_width)),
-      random_h(0, static_cast<int>(grid_height)) {
+      random_w(0, static_cast<int>(grid_width - 1)),
+      random_h(0, static_cast<int>(grid_height - 1)) {
   PlaceFood();
 }
 
