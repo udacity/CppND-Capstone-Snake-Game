cb2c09abcae31438ee77454cda2c844c322cfe02
Sebastian Hoffmann <sepphod@gmail.com>
2022-05-21 08:31:59 +0200

    some minor changes

--- src/game.cpp
+++ src/game.cpp
@@ -12,12 +12,12 @@ Game::Game(std::size_t grid_width, std::size_t grid_height)
 
 void Game::Run(Controller const &controller, Renderer &renderer,
                std::size_t target_frame_duration) {
-  Uint32 title_timestamp = SDL_GetTicks();
-  Uint32 frame_start;
-  Uint32 frame_end;
-  Uint32 frame_duration;
-  int frame_count = 0;
-  bool running = true;
+  Uint32 title_timestamp {SDL_GetTicks()};
+  Uint32 frame_start{0};
+  Uint32 frame_end{0};
+  Uint32 frame_duration{0};
+  int frame_count{0};
+  bool running {true};
 
   while (running) {
     frame_start = SDL_GetTicks();
@@ -31,11 +31,11 @@ void Game::Run(Controller const &controller, Renderer &renderer,
 
     // Keep track of how long each loop through the input/update/render cycle
     // takes.
-    frame_count++;
+    ++frame_count;
     frame_duration = frame_end - frame_start;
 
     // After every second, update the window title.
-    if (frame_end - title_timestamp >= 1000) {
+    if (frame_end - title_timestamp >= 1000U) {
       renderer.UpdateWindowTitle(score_, frame_count);
       frame_count = 0;
       title_timestamp = frame_end;
@@ -79,7 +79,7 @@ void Game::Update() {
     PlaceFood();
     // Grow snake_ and increase speed.
     snake_.GrowBody();
-    snake_.speed_ += 0.02;
+    snake_.speed_ += 0.02F;
   }
 }
 
