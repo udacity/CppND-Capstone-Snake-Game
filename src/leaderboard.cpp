e3626bdf086344bd4b4f4c91194404d5f409d057
Ryan Potter <ryan.potter94@gmail.com>
2024-02-16 20:46:22 +0000

    Fixed some typos and and added a header file for leaderboard.h.

--- src/leaderboard.cpp
+++ src/leaderboard.cpp
@@ -1,69 +1,67 @@
-#include <string>
-#include <vector>
-#include <algorithm>
+#include "leaderboard.h"
 
 class Record {
 
-	public:
-		Record(int s, std:string n) : score(s), name(n) {};
+public:
+	Record(int s, std::string n) : score(s), name(n) {};
 
-		Record(std::istream& is) {
-			is >> name >> score;
+	Record(std::istream& is) {
+		if (!(is >> name >> score)) {
+			throw std::runtime_error("Error reading from file");
 		}
+	}
 
-		~Record() {};
+	~Record() {};
 
-		Record(const Record& other) : score(other.getScore()), name(other.getName()) {};
+	Record(const Record& other) : score(other.getScore()), name(other.getName()) {};
 
-		Record& operator = (const Record& other) {
-			if (this != &other) {
-				score = other.score;
-				name = other.name;
-			}
-			return *this
+	Record& operator = (const Record& other) {
+		if (this != &other) {
+			score = other.score;
+			name = other.name;
 		}
+		return *this;
+	}
 
-		Record(Record&& other) : score(other.score), name(other.name) {
+	Record(Record&& other) : score(other.score), name(other.name) {
+		other.score = 0;
+		other.name = "";
+	}
+
+	Record& operator = (Record&& other) {
+		if (this != &other) {
+			score = other.score;
+			name = other.name;
 			other.score = 0;
 			other.name = "";
 		}
-
-		Record& operator = (Record&& other) {
-			if (this != &other) {
-				score = other.score;
-				name = other.name;
-				other.score = 0;
-				other.name = "";
-			}
-			return *this;
-		}
+		return *this;
+	}
 
 
-		int getScore() const { return score };
-		std::string getName() const { return name };
+	int getScore() const { return score; }
+	std::string getName() const { return name; }
 
-		bool operator<(const Record& other) const {
-			return score > other.getScore();
-		}
+	bool operator<(const Record& other) const {
+		return score > other.getScore();
+	}
 
-		void write(std::ostream& os) const {
-			os << name << " " << score << "\n";
-		}
+	void write(std::ostream& os) const {
+		os << name << " " << score << "\n";
+	}
 
-	private:
-		int score;
-		std:string name;
-
-	
+private:
+	int score;
+	std::string name;
 };
 
-class Leaderbord {
+class Leaderboard {
 
 public:
-	Leaderbord() {
+	Leaderboard() {
 		getRecords();
 	};
-	
+
 	void addRecord(Record& record) {
 		records.push_back(record);
 	}
@@ -73,25 +71,33 @@ public:
 	}
 
 	void getRecords() {
-		std::ifstream("leaderboard.txt");
-		while (file) {
-			records.push_back(Record(file));
+		std::ifstream file("leaderboard.txt");
+		if (file.is_open()) {
+			while (file) {
+				try
+					records.push_back(Record(file));
+			}
+			catch (std::runtime_error& e) {
+				std::cerr << "Error reading from file: " << e.what() << "\n";
+				break;
+			}
 		}
 	};
 
 	void saveRecords() {
-		std::ofstream("leaderboard.txt");
-		for( const auto& record : records) {
+		std::ofstream file("leaderboard.txt");
+		if (!file.is_open()) {
+			throw std::runtime_error("Failed to open leaderboard.txt for writing.")
+		}
+		for (const auto& record : records) {
 			record.write(file);
 		}
 	}
-		
-	);
 
 	void printRecords(int n) {
 		sortRecords();
-		for (int i =0; i<n ; i++) {
-			switch i{
+		for (int i = 0; i < n; i++) {
+			switch (i) {
 			case 0:
 				std::cout << "1st: "; break;
 			case 1:
@@ -102,15 +108,14 @@ public:
 				std::cout << i + 1 << "th: "; break;
 			}
 
-			record
+			std::cout << records[i].getName() << " " << records[i].getScore() << "\n";
 		}
 	}
 
 	void sortRecords() {
-		std::sort(records.begin(), records.end())
+		std::sort(records.begin(), records.end());
 	}
 
 private:
 	std::vector<Record> records;
-
 };
---
+++ src/leaderboard.h
@@ -0,0 +1,44 @@
+#ifndef LEADERBOARD_H
+#define LEADERBOARD_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+class Record {
+public:
+    Record(int s, std::string n);
+    Record(std::istream& is);
+    ~Record();
+    Record(const Record& other);
+    Record& operator = (const Record& other);
+    Record(Record&& other);
+    Record& operator = (Record&& other);
+
+    int getScore() const;
+    std::string getName() const;
+    bool operator<(const Record& other) const;
+    void write(std::ostream& os) const;
+
+private:
+    int score;
+    std::string name;
+};
+
+class Leaderboard {
+public:
+    Leaderboard();
+    void addRecord(Record& record);
+    void addRecord(Record&& record);
+    void getRecords();
+    void saveRecords();
+    void printRecords(int n);
+    void sortRecords();
+
+private:
+    std::vector<Record> records;
+};
+
+#endif // LEADERBOARD_H
